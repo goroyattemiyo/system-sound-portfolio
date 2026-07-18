0a1a7cf4b22e12427c17b853718bf0034a9e0cbd
Ggoro Yattemiyo <goro.money.business@gmail.com>
2026-07-18 18:59:31 +0900

    feat: generate sitemap during Astro build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://goroyattemiyo.github.io',
   base: '/system-sound-portfolio',
   output: 'static',
   trailingSlash: 'always',
+  integrations: [sitemap()],
 });
